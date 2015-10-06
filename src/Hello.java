37194077d355a8d6355c702e1c680e16b2ec4daf
Sugarcoder <mikichea@gmail.com>
2015-10-06 01:18:25 -0700

    HelloWorld HW1

--- src/Hello.java
+++ src/Hello.java
@@ -1,22 +1,10 @@
 
 public class Hello {
-    
+
     public static void main(String[] args) {
         System.out.println("Hello World\n" +
-                           "and Java Class!");
-
-        System.out.println("---------------------\n" +
-                            "|                   |\n" +
-                            "|                   |\n" +
-                            "|                   |\n" +
-                            "|                   |\n" +
-                            "|                   |\n" +
-                            "|                   |\n" +
-                            "|                   |\n" +
-                            "|                   |\n" +
-                             "---------------------\n");
-
-
+                "and Java Class!");
+        /*
 
         System.out.println("***       ***    ********      ***        ***           ***\n" +
                             "***       ***    ***           ***        ***        ***   ***\n" +
@@ -25,6 +13,39 @@ public class Hello {
                             "***       ***    ***           ***        ***        ***   ***\n" +
                             "***       ***    ***           ***        ***        ***   ***\n" +
                             "***       ***    ********      ********   ********      ***\n");
+
+        */
+
+
+        final int hSize = 30;
+        final int vSize = 30;
+
+
+        int count = 0;
+
+        while (count++ < hSize)
+            System.out.print("-");
+        System.out.printf("\n");
+
+
+        int count2 = 0;
+        while (count2++ < hSize - 2) {
+            System.out.println("|");
+
+            count = 0;
+            while (count++ < vSize - 2)
+                System.out.print(" ");
+            System.out.println("|");
+
+        }
+
+
+        count = 0;
+
+        while (count++ < hSize)
+            System.out.print("-");
+        System.out.printf("\n");
+
 
     }
 
