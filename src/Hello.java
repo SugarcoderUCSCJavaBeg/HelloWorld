74008d8119ffdf3c89cc25e6e25b54c6ca5cdce9
Sugarcoder <mikichea@gmail.com>
2015-10-06 08:16:07 -0700

    HelloWorld HW1

--- src/Hello.java
+++ src/Hello.java
@@ -4,19 +4,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World\n" +
                 "and Java Class!");
-        /*
-
-        System.out.println("***       ***    ********      ***        ***           ***\n" +
-                            "***       ***    ***           ***        ***        ***   ***\n" +
-                            "***       ***    ***           ***        ***        ***   ***\n" +
-                            "*************    ********      ***        ***        ***   ***\n" +
-                            "***       ***    ***           ***        ***        ***   ***\n" +
-                            "***       ***    ***           ***        ***        ***   ***\n" +
-                            "***       ***    ********      ********   ********      ***\n");
-
-        */
 
 
+        
         final int hSize = 30;
         final int vSize = 30;
 
