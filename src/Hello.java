7c48fad43a22d0a21a152a4e0b55f2dafba6ee5f
Sugarcoder <mikichea@gmail.com>
2015-10-07 23:13:44 -0700

    HelloWorld HW1 with other examples

--- src/Hello.java
+++ src/Hello.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Hello {
 
@@ -6,7 +8,7 @@ public class Hello {
                 "and Java Class!");
 
 
-        
+
         final int hSize = 30;
         final int vSize = 30;
 
@@ -19,11 +21,11 @@ public class Hello {
 
 
         int count2 = 0;
-        while (count2++ < hSize - 2) {
+        while (count2++ < hSize - 20) {
             System.out.println("|");
 
             count = 0;
-            while (count++ < vSize - 2)
+            while (count++ < vSize - 1)
                 System.out.print(" ");
             System.out.println("|");
 
@@ -35,6 +37,27 @@ public class Hello {
         while (count++ < hSize)
             System.out.print("-");
         System.out.printf("\n");
+
+
+        int randomNumber = new Random().nextInt(10) + 1;
+        System.out.println(randomNumber);
+
+
+
+        int age;
+
+        Scanner readInput = new Scanner(System.in);
+
+        System.out.print("Please enter your age: ");
+        age = readInput.nextInt();
+
+        if(age >= 20) {
+            System.out.println("You are old enough to drink");
+        } else if (age <= 20) {
+            System.out.println("You are not old enough to drink yet, wait one more year.");
+        } else {
+            System.out.println("You are too young to drink.");
+        }
 
 
     }
